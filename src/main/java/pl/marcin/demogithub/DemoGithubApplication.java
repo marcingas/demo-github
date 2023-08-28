8dbdde13fc11671d3140fff589ae4fea6022a52b
Marcin <gasiorek.mail@gmail.com>
2023-08-28 11:16:35 +0200

    Initial commit

---
+++ src/main/java/pl/marcin/demogithub/DemoGithubApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.demogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcin/demogithub/DemoGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.demogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
