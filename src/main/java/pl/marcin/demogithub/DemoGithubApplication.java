570a514470e65d6225a5ac236775334709974410
Marcin <gasiorek.mail@gmail.com>
2023-08-29 12:29:26 +0200

    final version

--- src/main/java/pl/marcin/demogithub/DemoGithubApplication.java
+++ src/main/java/pl/marcin/demogithub/DemoGithubApplication.java
@@ -3,11 +3,12 @@ package pl.marcin.demogithub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class DemoGithubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGithubApplication.class, args);
 	}
-
 }
